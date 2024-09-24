a2f6a60b27c130240e097b8ab18c4625b020ba2b
GGVidal <gabrielvidalgondim@gmail.com>
2024-09-24 12:14:41 -0300

    chore: creating routes

---
+++ src/routes/navigator.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Home } from "../screens/Home";
+import { Login } from "../screens/Login";
+import { Onboarding } from "../screens/Onboarding";
+import { RootStackParamList } from "./types";
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
---
+++ src/routes/types.ts
@@ -0,0 +1,5 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Onboarding: undefined;
+  Login: undefined;
+};
